169c24a023c79a31684bd57e4f3765c2eabf4575
JulioPlatero <115821183+JulioPlatero@users.noreply.github.com>
2024-06-24 15:03:57 -0600

    feat: add env CORS_ORIGINS in enviroments for deploy dynamics

--- src/utils/s3.helper.ts
+++ src/utils/s3.helper.ts
@@ -2,24 +2,24 @@ import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { environment } from '../constants/enviroment'
 
 export const client = new S3Client({
-    region: environment.awsRegion,
-    credentials: environment.awsCredentials,
-  })
+  region: environment.awsRegion,
+  credentials: environment.awsCredentials,
+})
 
 export const saveFileInS3 = async (NameKey: string, jsonData: any) => {
-    try {
+  try {
 
-        const jsonString = JSON.stringify(jsonData)
+    const jsonString = JSON.stringify(jsonData)
 
-        const parameters: any = {
-            Bucket: environment.awsBucket,
-            Key: `testing-lambda/${NameKey}`,
-            Body: jsonString,
-          }
-    
-        parameters.ContentType = 'application/json'
-        return await client.send(new PutObjectCommand(parameters))
-    } catch (error) {
-        return error
+    const parameters: any = {
+      Bucket: environment.awsBucket,
+      Key: `testing-lambda/${NameKey}`,
+      Body: jsonString,
     }
+
+    parameters.ContentType = 'application/json'
+    return await client.send(new PutObjectCommand(parameters))
+  } catch (error) {
+    return error
+  }
 }
